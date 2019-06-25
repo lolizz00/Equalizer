10085bbb3ca7215b0b5c6cf54e0cb8c0dfd06eab
lolizz00 <timofei.volkov@mail.ru>
2019-06-25 16:02:56 +0300

    finish manual mode

--- EQ_gui/MainWindow.py
+++ EQ_gui/MainWindow.py
@@ -7,6 +7,8 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QSystemTrayIcon
 
+import re
+import serial.tools.list_ports
 
 from serStm import serStm
 
@@ -17,9 +19,75 @@ class MW(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MW, self).__init__()
         self.setupUi(self)
         self.setCentralWidget(self.cw)
-
+        self.initSlots()
 
         self.ser = serStm()
+        self.ser.log_signal.connect(self.writeLog)
+
+        # DBG
+        self.ser.connect('COM10')
 
     def showErr(self, text):
         QtWidgets.QMessageBox.critical(self, 'Ошибка!', text)
+
+    def initSlots(self):
+        self.refrPortPushButton.clicked.connect(self.refrPortPushButtonClicked)
+        self.clearLogPushButton.clicked.connect(self.clearLogPushButtonClicked)
+        self.openPortPushButton.clicked.connect(self.openPortPushButtonClicked)
+        self.closePortPushButton.clicked.connect(self.closePortPushButtonClicked)
+        self.manResetPushButton.clicked.connect(self.manResetPushButtonClicked)
+        self.manConnPushButton.clicked.connect(self.manConnPushButtonClicked)
+        self.manReadPushButton.clicked.connect(self.manReadPushButtonClicked)
+        self.manWritePushButton.clicked.connect(self.manWritePushButtonClicked)
+
+    def writeLog(self, msg):
+        old_text = self.logTextEdit.toPlainText()
+
+        if old_text == '':
+            self.logTextEdit.setText(msg)
+        else:
+            self.logTextEdit.setText(old_text + '\n' + msg)
+
+
+
+    # ---
+
+    def manWritePushButtonClicked(self):
+        reg = int(self.manRegLineEdit.text(), 16)
+        val = int(self.manValLineEdit.text(), 16)
+
+        self.ser.stmWriteReg(reg, val)
+
+    def manReadPushButtonClicked(self):
+        reg = self.manRegLineEdit.text()
+        reg = int(reg, 16)
+        reg = self.ser.stmReadReg(reg)
+        self.manValLineEdit.setText(reg)
+
+    def manConnPushButtonClicked(self):
+        self.ser.stmConn()
+
+    def manResetPushButtonClicked(self):
+        self.ser.stmReset()
+
+    def closePortPushButtonClicked(self):
+        self.ser.disconnect()
+
+    def openPortPushButtonClicked(self):
+        port = self.portComboBox.currentText()
+        self.ser.connect(port)
+
+    def clearLogPushButtonClicked(self):
+        self.logTextEdit.setText('')
+
+    def refrPortPushButtonClicked(self):
+        prt = list(serial.tools.list_ports.comports())
+
+        sch = 1
+        self.writeLog('Описание доступных портов:')
+        for t in prt:
+            self.writeLog(str(sch) + ' : '  + t.description)
+            self.portComboBox.addItem(t.device)
+            sch = sch + 1
+
+
--- EQ_gui/serStm.py
+++ EQ_gui/serStm.py
@@ -1,19 +1,112 @@
 import serial
+import PyQt5.QtCore
+from PyQt5.QtCore import *
+import time
 
 
-class serStm:
+class serStm(PyQt5.QtCore.QObject):
+
+
+    log_signal =  pyqtSignal(str)
 
     def __init__(self):
+        super(serStm, self).__init__()
         self.ser = serial.Serial()
 
+    def log(self, msg):
+        self.log_signal.emit(msg)
+
     def connect(self, port):
-        pass
+        self.ser.port = port
+        self.ser.baudrate = 115200
+
+        try:
+            self.ser.open()
+        except:
+            self.log('Cannot open port ' +  port  + '!')
+            return
+
+        self.log('Port ' + port + ' succ. open')
 
     def disconnect(self):
-        pass
+        try:
+            self.ser.close()
+        except:
+            pass
 
-    def readReg(self):
-        pass
+    def _ask(self, txt):
+        self._write(txt)
+        time.sleep(0.015)
+        resp = self._read()
+        return resp
 
-    def writeReg(self):
-        pass
+    def _read(self):
+
+
+        vals = self.ser.read_all()
+        vals = vals.decode("utf-8")
+        vals = vals.replace('\r', '')
+        vals = vals.replace('\n', '')
+        return vals
+
+    def _write(self, txt):
+        txt = txt + '\n\r'
+        self.ser.write(txt.encode())
+        time.sleep(0.01)
+
+
+    def stmConn(self):
+
+        if not self.ser.is_open:
+            self.log('The port is not open!')
+            return
+
+        resp = self._ask('conn')
+        self.log('STM : ' + resp)
+
+    def stmReset(self):
+
+        if not self.ser.is_open:
+            self.log('The port is not open!')
+            return
+
+        resp = self._ask('reset')
+        self.log('STM : ' + resp)
+
+    def stmReadReg(self, reg):
+
+        if not self.ser.is_open:
+            self.log('The port is not open!')
+            return
+
+        reg = hex(reg).replace('0x', '')
+        ret = self._ask('read ' + reg)
+
+
+        if ret.find('ERR') != -1:
+            ret = 'ERROR'
+            self.log('Read error!')
+        else:
+            self.log('Read succ.')
+            ret = ret.split(' ')
+            ret = ret[2]
+
+        return ret
+
+
+
+    def stmWriteReg(self, reg, val):
+
+        if not self.ser.is_open:
+            self.log('The port is not open!')
+            return
+
+        reg = hex(reg).replace('0x', '')
+        val = hex(val).replace('0x', '')
+
+        msg = self._ask('write ' + reg + ' ' + val)
+
+        if msg.find('ERR') != -1:
+            self.log('STM : Error during writing!')
+        else:
+            self.log('STM : Writing successful')
